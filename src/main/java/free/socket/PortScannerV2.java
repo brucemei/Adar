7f41dfc75dc8da45c112d2c31abcde7c1d0cf439
whanche <whanche@126.com>
2015-10-26 13:55:52 +0800

    20151026

--- src/main/java/free/socket/PortScannerV2.java
+++ src/main/java/free/socket/PortScannerV2.java
@@ -4,16 +4,22 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
- * 端口扫描
+ * 端口扫描 v2.0
+ * 
+ * 采用多线程并发扫描
  */
-public class PortScanner {
+public class PortScannerV2 {
 	
 	/**
 	 * 连接超时 ms
 	 */
 	private static int TIME_OUT_CONNECT = 200;
+	
+	private static ExecutorService executor = Executors.newCachedThreadPool();
 	
 	public static void main(String[] args) {
 		scan("localhost", 8070, 10000);
@@ -28,18 +34,35 @@ public class PortScanner {
 		}
 		
 		for (int port = start; port < end; port++) {
-			Socket socket = null;
+			executor.execute(new Scanner(host, port));
+		}
+		
+		executor.shutdown();
+	}
+	
+	static class Scanner implements Runnable {
+		
+		private String host;
+		
+		private int port;
+		
+		public Scanner(String host, int port) {
+			super();
+			this.host = host;
+			this.port = port;
+		}
+
+		public void run() {
+			Socket socket = new Socket();
 			try {
 				System.out.println("Scan port...   Port: " + port);
 				
-				socket = new Socket();
 				socket.connect(new InetSocketAddress(host, port), TIME_OUT_CONNECT);
-				
 				System.err.println("Port is listen: " + port);
 			} catch (UnknownHostException e) {
 				System.out.println("Host wrong.");
 			} catch (IOException e) {
-				continue;
+				// do nothing
 			} finally {
 				if (socket != null) {
 					try {
