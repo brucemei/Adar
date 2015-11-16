71019a326f465e353a06a90319db6fba57cf6021
whanche <whanche@126.com>
2015-11-16 18:04:01 +0800

    20151116

--- src/main/java/free/socket/TestSocketServerClient.java
+++ src/main/java/free/socket/TestSocketServerClient.java
@@ -13,8 +13,24 @@ import java.net.Socket;
  */
 @SuppressWarnings("all")
 public class TestSocketServerClient {
+	
+	private static final String request = "POST /TestJavaEE/TestServlet1 HTTP/1.1\r\n" + 
+			 							  "Host: localhost:10080\r\n" + 
+			 							  "Connection: keep-alive\r\n" + 
+			 							  "Cache-Control: no-cache\r\n" + 
+			 							  "Content-Type: application/json\r\n" + 
+			 							  "Content-Length: 3\r\n" + 
+			 							  "\r\n" + 
+			 							  "123";
+	
+	private static final String response = "HTTP/1.1 200 OK\r\n" +
+			 							   "Connection: keep-alive\r\n" + 
+			 							   "Content-Length: 0\r\n" + 
+			 							   "\r\n" + 
+			 							   "";
+	
 	public static void main(String[] args) throws IOException {
-		testServer();
+		testClient();
 	}
 	
 	private static void testServer() throws IOException {
@@ -31,13 +47,7 @@ public class TestSocketServerClient {
 				int read = inputStream.read(buf);
 				System.out.println(new String(buf, 0, read));
 				
-				String out = "HTTP/1.1 200 OK\r\n" +
-							 "Connection: keep-alive\r\n" + 
-							 "Content-Length: 0\r\n" + 
-							 "\r\n" + 
-							 "";
-				
-				socket.getOutputStream().write(out.getBytes());
+				socket.getOutputStream().write(response.getBytes());
 			} catch (IOException e) {
 				continue;
 			}
@@ -48,32 +58,16 @@ public class TestSocketServerClient {
 		Socket socket = new Socket();
 		socket.connect(new InetSocketAddress("localhost", 8080));
 
-		long currentTimeMillis = System.currentTimeMillis();
-		
 		OutputStream outputStream = socket.getOutputStream();
 		InputStream inputStream = socket.getInputStream();
 		
-		String out = "POST /TestJavaEE/TestServlet1 HTTP/1.1\r\n" + 
-					 "Host: localhost:10080\r\n" + 
-					 "Connection: keep-alive\r\n" + 
-					 "Content-Length: 6\r\n" + 
-					 "Cache-Control: no-cache\r\n" + 
-					 "Content-Type: application/json\r\n" + 
-					 "Accept: */*\r\n" + 
-					 "Accept-Encoding: gzip, deflate\r\n" + 
-					 "Accept-Language: zh-CN,zh;q=0.8\r\n" + 
-					 "\r\n" + 
-					 "123";
-		
 		PrintStream printStream = new PrintStream(outputStream, true);
-		printStream.println(out);
+		printStream.print(request);
 		
 		byte[] buf = new byte[2024];
 		int read = inputStream.read(buf);
-		System.out.println(System.currentTimeMillis() - currentTimeMillis);
 		
 		System.out.println(new String(buf, 0, read));
-		System.out.println("over");
 		
 		socket.close();
 	}
