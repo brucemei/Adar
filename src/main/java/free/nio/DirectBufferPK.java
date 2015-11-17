23342518e09a72ffeedbcfd0a6185046aec6e02d
whanche <whanche@126.com>
2015-11-17 23:42:37 +0800

    20151117

---
+++ src/main/java/free/nio/DirectBufferPK.java
@@ -0,0 +1,35 @@
+package free.nio;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class DirectBufferPK {
+	
+	private static final String src = "E:/File_install/jprofiler_windows-x64_8_0_7.exe";
+	
+	private static final int size = 1024 * 1024 * 1024;
+	
+	public static void main(String[] args) throws IOException {
+		ByteBuffer buffer = ByteBuffer.allocate(size);
+		ByteBuffer directBuffer = ByteBuffer.allocateDirect(size);
+		
+		long start = System.currentTimeMillis();
+		read(src, buffer);
+		System.out.println("Buffer: " + (System.currentTimeMillis() - start));
+
+		start = System.currentTimeMillis();
+		read(src, directBuffer);
+		System.out.println("DirectBuffer: " + (System.currentTimeMillis() - start));
+	}
+	
+	private static void read(String src, ByteBuffer buffer) throws IOException {
+		RandomAccessFile srcFile = new RandomAccessFile(src, "r");
+		FileChannel srcChannel = srcFile.getChannel();
+		
+		srcChannel.read(buffer);
+		
+		srcFile.close();
+	}
+}
