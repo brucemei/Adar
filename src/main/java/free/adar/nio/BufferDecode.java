21648938502dbb777c44d68a30578858e343510a
Adar.w <adar-w@outlook.com>
2015-12-16 15:38:05 +0800

    Adar

--- src/main/java/free/adar/nio/BufferDecode.java
+++ src/main/java/free/adar/nio/BufferDecode.java
@@ -13,15 +13,19 @@ public class BufferDecode {
 	private static final String PATH = "E:/1.txt";
 	
 	public static void main(String[] args) throws IOException {
-		System.out.println(decode(ByteBuffer.wrap("hahaha哈哈".getBytes())));
+		System.out.println(decode(ByteBuffer.wrap("hahaha哈哈".getBytes()), false));
 
-		System.out.println(decode(readBuffer(PATH, false)));
+		System.out.println(decode(readBuffer(PATH, false), true));
 
-		System.out.println(decode(readBuffer(PATH, true)));
+		System.out.println(decode(readBuffer(PATH, true), true));
 	}
 	
-	private static String decode(ByteBuffer byteBuffer) throws IOException {
-		return Charset.forName(UTF8).newDecoder().decode(readBuffer(PATH, false)).toString();
+	private static String decode(ByteBuffer byteBuffer, boolean needFlip) throws IOException {
+		if (needFlip) {
+			byteBuffer.flip();
+		}
+		
+		return Charset.forName(UTF8).decode(byteBuffer).toString();
 	}
 
 	private static ByteBuffer readBuffer(String path, boolean isDirect) throws IOException {
