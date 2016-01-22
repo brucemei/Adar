d5e9b9700df03abbba5aed983ed7513710027621
Adar.w <adar-w@outlook.com>
2016-01-22 18:20:18 +0800

    Adar

--- src/main/java/free/adar/bugs/BugRemoveAll.java
+++ src/main/java/free/adar/bugs/BugRemoveAll.java
@@ -24,6 +24,7 @@ import java.util.Iterator;
 
 /**
  * Bugs
+ * 
  * @see free.adar.bugs.BugRemoveAll#removeAll(Collection, Collection)
  * 
  * @see java.util.AbstractCollection#removeAll(Collection)
@@ -33,13 +34,11 @@ public class BugRemoveAll {
 	public static void main(String[] args) throws Exception {
 		Collection<Bean> srcColl = srcColl(ArrayList.class);
 		Collection<Bean> delColl = delColl(HashSet.class);
-		
+
 		System.out.println("RemoveAll(JDK) 删除前:" + srcColl.size());
 		removeAll(srcColl, delColl);
 		System.out.println("RemoveAll(JDK) 删除后:" + srcColl.size());
 
-		
-		
 		Collection<Bean> srcColl2 = srcColl(ArrayList.class);
 		Collection<Bean> delColl2 = delColl(HashSet.class);
 
@@ -47,59 +46,61 @@ public class BugRemoveAll {
 		removeAllByAdar(srcColl2, delColl2);
 		System.out.println("RemoveAll(Adar) 删除后:" + srcColl2.size());
 	}
-	
+
 	/**
 	 * JDK实现
+	 * 
 	 * @see java.util.AbstractCollection#removeAll(Collection)
 	 */
-    public static boolean removeAll(Collection<?> c1, Collection<?> c2) {
-        boolean modified = false;
-        Iterator<?> it = c1.iterator();		// 此处建议遍历常规较小的待删除集合c2
-        while (it.hasNext()) {
-            if (c2.contains(it.next())) {	// 此处建议调用了c1的contains方法, 若c1为List, c2为Set,调用contains对象的不同将导致结果将不一致, 调用c1的contains方法更符合预期
-                it.remove();
-                modified = true;
-            }
-        }
-        return modified;
-    }
-    
+	public static boolean removeAll(Collection<?> c1, Collection<?> c2) {
+		boolean modified = false;
+		Iterator<?> it = c1.iterator(); // 此处建议遍历常规较小的待删除集合c2
+		while (it.hasNext()) {
+			if (c2.contains(it.next())) { // 此处建议调用了c1的contains方法, 若c1为List, c2为Set,调用contains对象的不同将导致结果将不一致
+										  // 调用作为this对象c1的contains方法更符合预期
+				it.remove();
+				modified = true;
+			}
+		}
+		return modified;
+	}
+
 	/**
 	 * Adar修改后实现
 	 */
-    public static boolean removeAllByAdar(Collection<?> c1, Collection<?> c2) {
-        boolean modified = false;
-        Iterator<?> it = c2.iterator();
-        while (it.hasNext()) {
-        	Object next = it.next();
-        	if (c1.contains(next)) {
-                c1.remove(next);
-                modified = true;
-            }
-        }
-        return modified;
-    }
-	
+	public static boolean removeAllByAdar(Collection<?> c1, Collection<?> c2) {
+		boolean modified = false;
+		Iterator<?> it = c2.iterator();
+		while (it.hasNext()) {
+			Object next = it.next();
+			if (c1.contains(next)) {
+				c1.remove(next);
+				modified = true;
+			}
+		}
+		return modified;
+	}
+
 	@SuppressWarnings("rawtypes")
 	private static Collection<Bean> srcColl(Class<? extends Collection> collType) throws Exception {
 		Collection<Bean> collection = buildColl(collType);
-		
+
 		collection.add(new Bean("1"));
 		collection.add(new Bean("2"));
 		collection.add(new Bean("3"));
-		
+
 		return collection;
 	}
-	
+
 	@SuppressWarnings("rawtypes")
 	private static Collection<Bean> delColl(Class<? extends Collection> collType) throws Exception {
 		Collection<Bean> collection = buildColl(collType);
-		
+
 		collection.add(new Bean("1"));
-		
+
 		return collection;
 	}
-	
+
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	private static Collection<Bean> buildColl(Class<? extends Collection> collType) throws Exception {
 		return collType.newInstance();
@@ -108,11 +109,11 @@ public class BugRemoveAll {
 	static class Bean {
 
 		private String id;
-		
+
 		private String name;
-		
+
 		private String value;
-		
+
 		public Bean(String id) {
 			this.id = id;
 		}
