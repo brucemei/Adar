c529b65a04fa3d7749235e6b23782fba12238d3d
Adar.w <adar-w@outlook.com>
2016-02-15 17:03:05 +0800

    Adar

--- src/main/java/pers/adar/zookeeper/zookeeper/Zookeeper.java
+++ src/main/java/pers/adar/zookeeper/zookeeper/Zookeeper.java
@@ -17,32 +17,70 @@
  */
 package pers.adar.zookeeper.zookeeper;
 
-import java.io.IOException;
-import java.util.concurrent.Phaser;
+import java.security.NoSuchAlgorithmException;
+import java.util.Collections;
+import java.util.concurrent.CountDownLatch;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooDefs.Ids;
+import org.apache.zookeeper.ZooDefs.Perms;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.ACL;
+import org.apache.zookeeper.data.Id;
+import org.apache.zookeeper.server.auth.DigestAuthenticationProvider;
 
 public class Zookeeper {
-
-	private static final String HOST = "192.168.0.107:2181,192.168.0.107:2182,192.168.0.107:2183";
 	
-	public static void main(String[] args) throws IOException {
+	private static final String HOST = "172.16.36.150";
+	
+	private static final String CONNECT;
+	
+	static {
+		CONNECT = String.format("%s:2181,%s:2182,%s:2183", HOST, HOST, HOST);
+	}
+	
+	public static void main(String[] args) throws Exception {
 		System.out.println(getClient().getState());
 	}
 	
-	public static ZooKeeper getClient() throws IOException {
-		Phaser phaser = new Phaser(1);
+	public static ZooKeeper getClient() throws Exception {
+		return getClient(null, null);
+	}
+	
+	public static ZooKeeper getClient(String scheme, byte[] auth) throws Exception {
+		CountDownLatch latch = new CountDownLatch(1);
 		
-		ZooKeeper zooKeeper = new ZooKeeper(HOST, 5000, (e) -> {
-			phaser.arrive();
+		ZooKeeper zooKeeper = new ZooKeeper(CONNECT, 5000, (e) -> {
+			latch.countDown();
 		});
+			
+		latch.await();
 		
-		phaser.awaitAdvance(phaser.getPhase());
+		if (StringUtils.isNotEmpty(scheme) && auth != null) {
+			zooKeeper.addAuthInfo(scheme, auth);
+		}
 		
 		return zooKeeper;
+	}
+	
+	public static void auth(ZooKeeper zooKeeper) throws NoSuchAlgorithmException, KeeperException, InterruptedException {
+		ACL acl = new ACL(Perms.READ, new Id("digest", DigestAuthenticationProvider.generateDigest("admin:admin")));
+		
+		zooKeeper.create("/auth", "auth".getBytes(), Collections.singletonList(acl), CreateMode.EPHEMERAL);
+	}
+	
+	public static void authForRoot(ZooKeeper zooKeeper) throws NoSuchAlgorithmException, KeeperException, InterruptedException {
+		ACL acl = new ACL(Perms.ALL, new Id("digest", DigestAuthenticationProvider.generateDigest("admin:admin")));
+		
+		zooKeeper.setACL("/", Collections.singletonList(acl), -1);
+	}
+	
+	public static void createNodeWithAuth(ZooKeeper zooKeeper, String path, String username, String password) throws KeeperException, InterruptedException, NoSuchAlgorithmException {
+		ACL acl = new ACL(Perms.READ | Perms.WRITE | Perms.CREATE | Perms.DELETE, new Id("digest", DigestAuthenticationProvider.generateDigest(username + ":" + password)));
+
+		zooKeeper.create(path, path.getBytes(), Collections.singletonList(acl), CreateMode.PERSISTENT);
 	}
 	
 	public static void createNode(ZooKeeper zooKeeper) throws KeeperException, InterruptedException {
@@ -50,5 +88,21 @@ public class Zookeeper {
 		zooKeeper.create("/test2", "test".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
 		zooKeeper.create("/test3", "test".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
 		zooKeeper.create("/test4", "test".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_SEQUENTIAL);
+	}
+	
+	public static void crud(ZooKeeper zooKeeper) throws Exception {
+		zooKeeper.create("/test", "test".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+		zooKeeper.delete("/test", -1);
+		zooKeeper.setData("/test", "test".getBytes(), -1);
+		zooKeeper.getData("/test", (e) -> {
+			System.out.println(e);
+		}, null);
+		
+		zooKeeper.exists("/test", (e) -> {
+			System.out.println(e);
+		});
+		zooKeeper.getChildren("/test", (e) -> {
+			System.out.println(e);
+		});
 	}
 }
